34474ade205e60197fca755a33f73432b33688f1
CoderN-P <neel.parpia@gmail.com>
2024-02-03 09:32:27 -0800

    Initial Commit

---
+++ EventSync/EventSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventSyncApp.swift
+//  EventSync
+//
+//  Created by GreatNeel on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EventSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
